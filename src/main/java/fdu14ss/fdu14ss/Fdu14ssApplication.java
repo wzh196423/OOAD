6f5b25eef0e82da5adcd15173b0edcdd3624be81
wzh196423 <570251170@qq.com>
2017-06-17 14:44:53 +0800

    first init

---
+++ src/main/java/fdu14ss/fdu14ss/Fdu14ssApplication.java
@@ -0,0 +1,12 @@
+package fdu14ss.fdu14ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fdu14ssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fdu14ssApplication.class, args);
+	}
+}
---
+++ src/test/java/fdu14ss/fdu14ss/Fdu14ssApplicationTests.java
@@ -0,0 +1,16 @@
+package fdu14ss.fdu14ss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Fdu14ssApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
